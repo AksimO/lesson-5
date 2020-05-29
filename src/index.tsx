9bf1a3b2aacff8d70d1444d93a2dd23de6409455
Maksim <m.romanenko@snaswap.eu>
2020-05-29 21:56:47 +0200

    React boilerplate

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import ReactDom from "react-dom";
+import { App } from "./App";
+
+const divRoot = document.getElementById("root");
+
+ReactDom.render(<App />, divRoot);
